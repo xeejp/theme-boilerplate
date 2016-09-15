ab591f5dfc77c209b5a182d72c748c9863fc6190
Ryo Hashiguchi <ryo12redstone@gmail.com>
2016-09-15 09:34:31 +0900

    Fix

--- host/index.js
+++ host/index.js
@@ -1,23 +1,7 @@
-import { fork, take, call } from 'redux-saga/effects'
-
 import App from './App.js'
 import saga from './saga'
 import reducer from './reducer'
-import { openParticipantPage } from './actions'
 
 import startApp from 'shared/index.js'
 
-// Saga
-function* openParticipantPageSaga() {
-  while (true) {
-    const { payload: id } = yield take(`${openParticipantPage}`)
-    yield call(_experiment.openParticipantPage.bind(_experiment), id)
-  }
-}
-
-function* hostSaga() {
-  yield fork(saga)
-  yield fork(openParticipantPageSaga)
-}
-
-startApp(App, reducer, hostSaga)
+startApp(App, reducer, saga, true)
--- shared/index.js
+++ shared/index.js
@@ -7,12 +7,17 @@ import createSagaMiddleware from 'redux-saga'
 import { createAction, createReducer } from 'redux-act'
 import reduceReducers from 'reduce-reducers'
 import { create as createJsondiffpatch } from 'jsondiffpatch'
+import clone from 'clone'
+import { fork, take, call } from 'redux-saga/effects'
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
-injectTapEventPlugin();
 
-export default function startApp(component, reducer, saga) {
-  const logger = createLogger();
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
+import { openParticipantPage } from 'host/actions'
+
+export default function startApp(component, reducer, saga, host=false) {
+  const logger = createLogger()
   const sagaMiddleware = createSagaMiddleware()
 
   let middlewares = [sagaMiddleware, logger]
@@ -20,7 +25,7 @@ export default function startApp(component, reducer, saga) {
   const patch = createAction('patch', diff => diff)
   const jsondiffpatch = createJsondiffpatch({})
   const patcher = createReducer({
-    [patch]: (state, diff) => jsondiffpatch.patch(Object.assign({}, state), diff)
+    [patch]: (state, diff) => jsondiffpatch.patch(clone(state), diff)
   })
 
   const store = createStore(
@@ -28,7 +33,23 @@ export default function startApp(component, reducer, saga) {
     applyMiddleware(...middlewares)
   )
 
-  sagaMiddleware.run(saga)
+  if (host) {
+    // Saga
+    function* openParticipantPageSaga() {
+      while (true) {
+        const { payload: id } = yield take(`${openParticipantPage}`)
+        yield call(_experiment.openParticipantPage.bind(_experiment), id)
+      }
+    }
+
+    function* hostSaga() {
+      yield fork(saga)
+      yield fork(openParticipantPageSaga)
+    }
+    sagaMiddleware.run(hostSaga)
+  } else {
+    sagaMiddleware.run(saga)
+  }
 
   var _experiment = new Experiment(_topic, _token);
 
